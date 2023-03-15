d4e50a3753c64c44dcd6b6a969d32e153fc2e996
Aakash928 <asassss337@gmail.com>
2023-03-15 20:11:22 +0530

    change

--- index.js
+++ index.js
@@ -1,19 +1,8 @@
-const fs = require("fs");
-const os = require("os");
-const path = require("path");
-
 const jsonServer = require('json-server')
-const server = jsonServer.create();
-const router = jsonServer.router(path.resolve(os.tmpdir() + "/db.json"));
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
  
-
-
-
-fs.copyFile("db.json", os.tmpdir() + "/db.json", function (err) {
-  if (err) console.log(err);
-  else console.log("copy file succeed to" + os.tmpdir());
-});
 server.use(middlewares)
 server.use('', router)
 server.listen(process.env.PORT || 5000, () => {
