b5d28dbd6731513d5bb91db1773c5f9203810d4e
Aakash928 <asassss337@gmail.com>
2023-04-25 11:19:24 +0530

    server-update

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
-const port = process.env.PORT || 3001; 
-
-server.use(middlewares);
-server.use(router);
-
-server.listen(port);
+const PORT =process.env.PORT ||3001
+const path=require('path')
+const jsonServer=require('json-server')
+const server=jsonServer.create()
+const router=jsonServer.router
+(path.join(__dirname,"db.json"))
+const middlewares=jsonServer.defaults()
+server.use(middlewares)
+server.use(jsonServer.bodyParser)
+server.use('/api', router)
+server.listen(PORT,()=>{
+    console.log("server is running at port ");
+})
